a36fef62f3ddd38582136cc864060a1db9fb649c
PurplePanther <45367470+PurplePanther@users.noreply.github.com>
2022-08-13 22:55:57 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Nav from './Nav';
 import SomeContents from './SomeContent';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 //import Message from './Message';
 
 function App() {
