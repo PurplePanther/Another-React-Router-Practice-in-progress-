9f0bc03e4fecfbc0ba324d760d6a4066dfdeb62a
PurplePanther <45367470+PurplePanther@users.noreply.github.com>
2022-08-15 16:26:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 //import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Nav from './Nav';
-import SomeContents from './SomeContent';
+//import SomeContents from './SomeContent';
 import About from './About';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-//import Message from './Message';
+import Message from './Message';
+import NotFound from './NotFound';
+
+//Route not working
+//is there a new react-router-dom version?
+//version 6 now
+
+//NOTES FOR MYSELF ABOUT v6:
+//Switch is now Routes
+//Route format is now <Route path='/about' element={<About/>}/> instead of <Route path='/about' component={About}>
+//element instead of component
+//what about 'exact'? Not needed anymore
 
 function App() {
   return (
     <Router>
-      <div className="App">
-          <div>
-            <Nav/>
-            <SomeContents/>
-          </div>
-      </div>
+      <div>
+          <Message/>
+          <Nav/>
+          {/*<SomeContents/>*/}
+          <Routes>
+            <Route path='/about' element={<About/>}/>
+            {/*<Route path='/minkydinkred' element={<MinkydinkRed/>}/>*/}
+            <Route path="*" element={<NotFound/>}/>
+          </Routes>
+          
+        </div>
     </Router>
   );
 }
